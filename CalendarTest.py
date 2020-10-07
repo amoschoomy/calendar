74e429273d87c053d06b0ba9d21513efeaf8734d
amoschoomy <acho0057@student.monash.edu>
2020-10-07 22:35:50 +0800

    Created test cases for viewing upcoming reminders, updated yml script to include branch coverage

--- CalendarTest.py
+++ CalendarTest.py
@@ -82,12 +82,107 @@ class CalendarTestViewUpcomingEvents(unittest.TestCase):
         ]}
         self.assertEqual(Calendar.get_upcoming_events_2(api,ex_time),"test,2020-10-03T02:00:00.000000Z")
 
+class CalendarGetUpcomingReminders(unittest.TestCase):
+    def test_get_upcoming_reminders_invalid_date(self):
+        #Path 1 where execution stops after invalid date given
+        ex_time="January 1 2020" #Date is of an invalid date so will throw Value Error
+        mock_api=Mock() #Mock api
+        with self.assertRaises(ValueError):
+            Calendar.get_upcoming_reminders(mock_api,ex_time)
+
+    @patch("Calendar.get_calendar_api")
+    def test_get_upcoming_reminders_path2(self,api):
+
+        #Path 2 where the if statement of date validation succeeds, outer for loop is executed, else branch is executed
+        #inside the loop, and from there the for loop in else branch is executed
+        ex_time="2020-10-03T00:00:00.000000Z"
+        api.events.return_value.list.return_value.execute.return_value = {
+        "items": [
+                    {
+                        "summary": "test",
+                        "start": {
+                            "dateTime": "2020-10-03T02:00:00.000000Z"
+                        },
+                        "end": {
+                            "dateTime": "2020-10-03T02:45:00.000000Z"
+                        },"reminders":{
+                            'useDefault': False,
+                            'overrides': [
+                                        {'method': 'email', 'minutes': 1},
+                                        {'method': 'popup', 'minutes': 10},
+    ],},
+                    },
+                       
+        ]}
+        reminders=Calendar.get_upcoming_reminders(api,ex_time)
+        # Two seperate assertion checks for each method of reminder is present in method output
+        self.assertIn("email 1",reminders)
+        self.assertIn("popup 10",reminders)
+
+    @patch("Calendar.get_calendar_api")
+    def test_get_upcoming_reminders_path3(self,api):
+
+        #Path 3 where the if branch of date validity succeeded, the outer for loop doesn't get executed.
+        ex_time="2020-10-03T00:00:00.000000Z"
+        api.events.return_value.list.return_value.execute.return_value ={}
+        reminders=Calendar.get_upcoming_reminders(api,ex_time)
+        self.assertEqual(reminders,"")
+
+
+    @patch("Calendar.get_calendar_api")
+    def test_get_upcoming_reminders_path4(self,api):
+        #Path 4 where if branch of date validity succeeded, outer for loop is executed, else branch is executed but the for
+        #loop in else branch is not executed
+        ex_time="2020-10-03T00:00:00.000000Z"
+        api.events.return_value.list.return_value.execute.return_value = {
+        "items": [
+                    {
+                        "summary": "test",
+                        "start": {
+                            "dateTime": "2020-10-03T02:00:00.000000Z"
+                        },
+                        "end": {
+                            "dateTime": "2020-10-03T02:45:00.000000Z"
+                        },"reminders":{
+                            'useDefault': False,
+                            'overrides': [
+                                            ],},
+                    },
+        
+        ]}
+        reminders=Calendar.get_upcoming_reminders(api,ex_time)
+        self.assertEqual(reminders,"\n") #A newline is returned due to the outer for loop being executed
+
+    @patch("Calendar.get_calendar_api")
+    def test_get_upcoming_reminders_path5(self,api):
+        #Path 5 where if statement to check date validity succeeds, outer for loop is executed, and if
+        #branch is executed which is default reminders
+        ex_time="2020-10-03T00:00:00.000000Z"
+        api.events.return_value.list.return_value.execute.return_value = {
+        "items": [
+                    {
+                        "summary": "test",
+                        "start": {
+                            "dateTime": "2020-10-03T02:00:00.000000Z"
+                        },
+                        "end": {
+                            "dateTime": "2020-10-03T02:45:00.000000Z"
+                        },"reminders":{
+                            'useDefault': True,
+                            'overrides': [
+                                            ],},
+                    },
+        
+        ]}
+        reminders=Calendar.get_upcoming_reminders(api,ex_time)
+        self.assertEqual(reminders,"test,Reminder through popup 10 minutes before event starts\n")
+
 
 
 
 def main():
     # Create the test suite from the cases above.
-    test_classes=[CalendarTest,CalendarTestViewUpcomingEvents] #Test 
+    test_classes=[CalendarTest,CalendarTestViewUpcomingEvents,CalendarGetUpcomingReminders] #Test Classes 
     for classes in test_classes:
         suite = unittest.TestLoader().loadTestsFromTestCase(classes)
         # This will run the test suite.
