0b715a229463ec0a48cee39e95352cb99359a628
Andrew <apan0027@student.monash.edu>
2020-10-10 01:34:15 +0800

    Added Test Search Reminders Class

--- CalendarTest.py
+++ CalendarTest.py
@@ -507,7 +507,74 @@ class CalenderTestSearchEvents(unittest.TestCase):
         self.assertIn(query, searched_events)
 
 
+class TestSearchReminders(unittest.TestCase):
 
+    def test_none_reminder_query(self, api):
+        query = None
+
+        with self.assertRaises(TypeError):
+            Calendar.get_searched_reminders(api, query)
+
+    def test_empty_reminder_query(self, api):
+        query = ""
+
+        with self.assertRaises(ValueError):
+            Calendar.get_searched_reminders(api, query)
+
+        query = " "
+
+        with self.assertRaises(ValueError):
+            Calendar.get_searched_reminders(api, query)
+
+    def test_no_reminders(self):
+        api = Mock()
+        api.events.return_value.list.return_value.execute.return_value = {}
+
+        query = "John"
+
+        reminders = Calendar.get_searched_reminders(api, query)
+        self.assertEqual(reminders, "")
+
+    def test_reminders_found(self):
+        query = "testing this string"
+
+        api = Mock()
+        api.events.return_value.list.return_value.execute.return_value = {
+            "items": [
+                {
+                    "summary": query,
+                    "reminders": {
+                        'useDefault': False,
+                        'overrides': [
+                            {'method': 'email', 'minutes': 1},
+                            {'method': 'popup', 'minutes': 10},
+                        ], },
+                },
+            ]}
+
+        reminders = Calendar.get_upcoming_reminders(api, query)
+        self.assertEqual(api.events.return_value.list.return_value.execute.call_count, 1)
+        self.assertIn("email 1", reminders)
+        self.assertIn("popup 10", reminders)
+
+    def test_reminders_found_default(self):
+
+        query = "testing this string"
+        api = Mock()
+        api.events.return_value.list.return_value.execute.return_value = {
+            "items": [
+                {
+                    "summary": query,
+                    "reminders": {
+                        'useDefault': True,
+                        'overrides': [], },
+                },
+
+            ]}
+
+        reminders = Calendar.get_searched_reminders(api, query)
+        self.assertEqual(api.events.return_value.list.return_value.execute.call_count, 1)
+        self.assertEqual(reminders, query+",Reminder through popup 10 minutes before event starts\n")
 
 
 
