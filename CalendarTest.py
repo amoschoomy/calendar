4dd8edcafe5a4d931709843827bf53e9e83b7fb4
amoschoomy <acho0057@student.monash.edu>
2020-10-10 21:48:11 +0800

    deleted throwaway test cases

--- CalendarTest.py
+++ CalendarTest.py
@@ -6,33 +6,6 @@ import Calendar
 
 
 # Add other imports here if needed
-
-class CalendarTest(unittest.TestCase):
-    # This test tests number of upcoming events.
-    def test_get_upcoming_events_number(self):
-        num_events = 2
-        time = "2020-08-03T00:00:00.000000Z"
-
-        mock_api = Mock()
-        events = Calendar.get_upcoming_events(mock_api, time, num_events)
-
-        self.assertEqual(
-            mock_api.events.return_value.list.return_value.execute.return_value.get.call_count, 1)
-
-        args, kwargs = mock_api.events.return_value.list.call_args_list[0]
-        self.assertEqual(kwargs['maxResults'], num_events)
-
-    # Add more test cases here
-    # This test tests for assertion error raised if number of events
-    def test_get_upcoming_events_negative_number(self):
-        num_events = -1
-        ex_time = "2020-08-03T00:00:00.000000Z"
-
-        mock_api = Mock()
-        with self.assertRaises(ValueError):
-            Calendar.get_upcoming_events(mock_api, ex_time, num_events)
-
-
 class CalendarTestGetUpcomingEvents(unittest.TestCase):
     # Test Suite for User Story 1
 
