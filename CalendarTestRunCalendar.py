afbf052652914b77942ba61bd4fae306e62dc661
Andrew <apan0027@student.monash.edu>
2020-10-12 01:13:40 +0800

    Added test canse for search reminders in the CLI

--- CalendarTestRunCalendar.py
+++ CalendarTestRunCalendar.py
@@ -266,7 +266,31 @@ class CalendarTestRunCalendar(unittest.TestCase):
             Calendar.run_calendar(api)
             self.assertIn("Monash Exam,2020-10-03T02:00:00.000000Z", mocked_output.getvalue())
 
-
+        @patch("Calendar.get_calendar_api")
+        @patch('sys.stdout', new_callable=StringIO)
+        @patch('Calendar.input', create=True)
+        def test_run_calendar_search_reminders(self, mocked_input, mocked_output, api):
+            api.events.return_value.list.return_value.execute.return_value = {
+                "items": [
+                    {
+                        "summary": "Monash Exam",
+                        "start": {
+                            "dateTime": "2020-10-03T02:00:00.000000Z"
+                        },
+                        "end": {
+                            "dateTime": "2020-10-03T02:45:00.000000Z"
+                        }, "reminders": {
+                        'useDefault': False,
+                        'overrides': [
+                            {'method': 'email', 'minutes': 1},
+                            {'method': 'popup', 'minutes': 10},
+                        ]}
+                    }
+                ]}
+            mocked_input.side_effect = ["search -r", "Monash Exam", "exit"]
+            Calendar.run_calendar(api)
+            self.assertIn("email,1", mocked_output.getvalue())
+            self.assertIn("popup,10", mocked_output.getvalue())
 
 
 def main():
